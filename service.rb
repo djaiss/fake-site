98f4a4c2ebb014fdfdb49aa2284da713007dd75b
Regis Freyd <githubrfreyd@fastmail.fm>
2015-12-09 13:22:40 -0500

    Less randomness

--- service.rb
+++ service.rb
@@ -3,15 +3,15 @@ require 'sinatra'
 get '/' do
 
   alphabet = ('a'..'e').to_a
-  countries = ['.fr', '.com', '.gov']
+  countries = ['.fr']
 
-  iteration = Random.rand(1...5)
+  iteration = Random.rand(2...5)
 
   i = 0
   urls = Array.new
   
   begin
-    url = 'http://' + alphabet[Random.rand(1...5)] + countries[Random.rand(1...3)]
+    url = 'http://' + alphabet[Random.rand(1...5)] + countries[Random.rand(1...1)]
     urls << url
     i +=1
   end until i == iteration
