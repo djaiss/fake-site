842931d46bb98c4e79fe9b9da80524cca682d20c
Regis Freyd <githubrfreyd@fastmail.fm>
2015-12-10 09:25:34 -0500

    Add more links on the page

--- service.rb
+++ service.rb
@@ -5,17 +5,17 @@ get '/' do
   alphabet = ('a'..'e').to_a
   countries = ['.fr']
 
-  iteration = Random.rand(2...5)
+  iteration = Random.rand(3...8)
 
   i = 0
   urls = Array.new
-  
+
   begin
     url = 'http://' + alphabet[Random.rand(1...5)] + countries[0]
     urls << url
     i +=1
   end until i == iteration
-  
+
   @urls = urls
   erb :index
 
